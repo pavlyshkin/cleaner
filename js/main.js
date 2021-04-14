0dd1deb25cc559ea2a8b436f5f62d7d583368f17
pavlyshkin <pasch.fedorow2013@yandex.ru>
2021-04-14 23:16:00 +0300

    last fix

--- js/main.js
+++ js/main.js
@@ -13,6 +13,23 @@ $(document).ready(function () {
       },
     },
   });
-  })
+  });
   $('.phone').mask('+0(000)000-00-00', {placeholder: "телефон"});
+
+
+
+
+    var count = 0;
+
+ $('#counter').data('count', 0);
+$('#update').click(function(){
+    $('#counter').html(function(){
+        var $this = $(this),
+            count = $this.data('count') + 1;
+
+        $this.data('count', count);
+        return count;
+    });
+});
+
 });
